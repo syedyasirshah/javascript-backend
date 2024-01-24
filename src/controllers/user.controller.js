33aa364885eec4c6cb8ea631cc2c64efedac6dca
syedyasirshah <yasirshab@gmail.com>
2024-01-24 22:13:01 +0500

    Add getUserChannelProfile api and write some aggregation pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -279,3 +279,72 @@ exports.updateUserCoverImage = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, user, "cover image updated successfully"));
 });
+exports.getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) {
+    throw new ApiError(400, "username not found");
+  }
+
+  const channel =
+    await User.aggregate[
+      ({
+        $match: {
+          username: username?.toLowerCase(),
+        },
+      },
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers",
+        },
+      },
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "subscribedTo",
+        },
+      },
+      {
+        $addFields: {
+          subscribersCount: {
+            $size: "$subscribers",
+          },
+          subscribedToCount: {
+            $size: "$subscribedTo",
+          },
+          isSubscribed: {
+            $cond: {
+              if: {
+                $in: [req.user?._id, "$subscribers.subscriber"],
+              },
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          fullName: 1,
+          subscribersCount: 1,
+          subscribedToCount: 1,
+          username: 1,
+          avatar: 1,
+          coverImage: 1,
+          email: 1,
+        },
+      })
+    ];
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exist.");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "User channel found"));
+});
